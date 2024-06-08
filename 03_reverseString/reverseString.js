2b1a8554ed455ccdc622986b2fc3f8dc0f8a47c9
Ezequiel Kuchar <ezequiel.kuchar@gmail.com>
2024-06-08 21:24:06 +0200

    Exercises 1, 2 and 3 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(phrase) {
+    myArray = []
 
+    for (i = 0; i < phrase.length; i++){
+        myArray[i] = phrase[i]
+    }
+
+    reversedPhrase = myArray.toReversed().join("")
+    return reversedPhrase
 };
 
 // Do not edit below this line
