6f0bdc4aa4a9a77bae5ff34e2b34539e84768f64
EzequielKuchar <ezequiel.kuchar@gmail.com>
2024-06-13 17:41:47 +0200

    Exercise 4 and 5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(numero1, numero2) {
+    if (numero1 < 0 || numero2 < 0 || !Number.isInteger(numero1) || !Number.isInteger(numero2)) {
+        return 'ERROR';
+    };
 
+    let start = Math.min(numero1, numero2);
+    let end = Math.max(numero1, numero2);
+    let suma = 0;
+
+    for (let i = start; i <= end; i++) {
+        suma += i;
+    }
+
+    return suma;
 };
 
 // Do not edit below this line
